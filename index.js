1fd145f50133fbd2411cd75627154a19424a700a
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-10 13:18:39 +0200

    Added task 4

--- index.js
+++ index.js
@@ -32,9 +32,41 @@ console.log(typeof res4); // ""number""
 
 // Task 3 //
 const question = prompt('Ви досягли повнолітнього віку?');
-if(question >= 18) {
+if (question >= 18) {
     alert('Ви досягли повнолітнього віку');
-}else {
+} else {
     alert('Ви ще надто молоді');
 }
 
+// Task 4 //
+let arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5];
+// Підрахунок к-сті входжень кожного числа 
+let count = {};
+for (let i = 0; i < arr.length; i++) {
+    if (count[arr[i]]) {
+        count[arr[i]]++;
+    } else {
+        count[arr[i]] = 1;
+    }
+}
+// Знаходження числа, яке найбільш часто входить у масив
+let maxCount = 0;
+let data;
+for (key in count) {
+    if (count[key] > maxCount) {
+        maxCount = count[key];
+        data = key;
+    }
+}
+// Створення нового масиву, що містить число, яке найбільш часто входить у масив
+let newDataArr = [parseInt(data)];
+// Видалення всіх входжень числа з поточного масиву
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === parseInt(data)) {
+        arr.splice(i, 1);
+        i--;
+    }
+}
+
+console.log(newDataArr);
+console.log(arr);
